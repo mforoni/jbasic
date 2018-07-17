e64bdb67d8f6558c6af9d2e62f6067914385bdf8
marco <marcofor10@gmail.com>
2018-07-17 10:07:57 +0200

    fix potential null

--- jbasic/src/main/java/com/github/mforoni/jbasic/time/Patterns.java
+++ jbasic/src/main/java/com/github/mforoni/jbasic/time/Patterns.java
@@ -3,6 +3,7 @@ package com.github.mforoni.jbasic.time;
 import javax.annotation.Nonnull;
 import com.github.mforoni.jbasic.JEnums;
 import com.google.common.base.Function;
+import javax.annotation.Nullable;
 
 /**
  * Represents a set of {@code Pattern}. Some of them are provided as {@code public} {@code static}
@@ -19,9 +20,10 @@ public class Patterns {
   public static final Patterns DAY_MONTH_YEAR_NUMERIC =
       new Patterns(Pattern.DD_MM_YYYY_HYPEN, Pattern.DD_MM_YYYY_SLASH, Pattern.DDMMYYYY);
   public static final Function<Pattern, String> TO_DATE_FORMAT = new Function<Pattern, String>() {
+    @Nullable
     @Override
-    public String apply(@Nonnull final Pattern pattern) {
-      return pattern.get();
+    public String apply(@Nullable final Pattern pattern) {
+      return pattern != null ? pattern.get() : null;
     }
   };
   private final Pattern[] patterns;
