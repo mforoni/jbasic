29363c8c102d43bcd949eb80430255d864820414
mforoni <marcofor10@gmail.com>
2018-10-13 17:13:20 +0200

    add beta annotation

--- jbasic/src/main/java/com/github/mforoni/jbasic/io/JPaths.java
+++ jbasic/src/main/java/com/github/mforoni/jbasic/io/JPaths.java
@@ -6,11 +6,13 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.annotation.Nonnull;
+import com.google.common.annotations.Beta;
 import com.google.common.base.Optional;
 
 /**
  * @author Foroni Marco
  */
+@Beta
 public final class JPaths {
   private JPaths() {
     throw new AssertionError();
